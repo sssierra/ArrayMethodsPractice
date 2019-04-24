67fc13c15975472d0c12ad41413da7c597c3e908
sssierra <44619154+sssierra@users.noreply.github.com>
2019-04-24 10:50:08 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var grateful = ["food", "water", "time"];
+grateful.pop();
+console.log(grateful);
+
+
+grateful.push("family", "sports");
+console.log(grateful);
+
+grateful.shift();
+console.log(grateful);
+
+grateful.unshift("music");
+console.log(grateful);
+
