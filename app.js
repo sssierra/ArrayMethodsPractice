697234d99804db57db044b118f203794d3fe44fe
sssierra <44619154+sssierra@users.noreply.github.com>
2019-04-24 10:59:18 -1000

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ console.log(grateful);
 grateful.unshift("music");
 console.log(grateful);
 
+
+
+var proud = ["attendance", "perserverence"];
+proud.pop();
+console.log(proud);
+
+proud.push("sports", "hobbies");
+console.log(proud);
+
+proud.shift();
+console.log(proud);
+
+proud.unshift("patience");
+console.log(proud);
